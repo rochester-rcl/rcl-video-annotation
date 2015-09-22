df04ba64d5b605c15dcd3074ab680f63c5d495a4
JoshR <jromphf@library.rochester.edu>
2015-09-21 23:34:00 -0400

    cleaned up html return in form.php

--- php/controllers/form.php
+++ php/controllers/form.php
@@ -12,18 +12,17 @@ if ($postAction == 'getForm') {
 
   $myForm = MarkerTypeDAO::getMarkerForm();
 
-
   foreach ($myForm as $form){
+    
       $cat = $form['category'];
       $markerCode = $form['marker_code'];
       $name = $form['name'];
       $desc = $form['description'];
 
-
       echo '<button value="' . $markerCode . '" class="' . $cat . '" title="' . $desc . '">' . $name . '</button>';
 
     }
-  
+
   echo '<a href="#" id="insertMarkerButton">Insert Marker</a>';
 
 } else {
