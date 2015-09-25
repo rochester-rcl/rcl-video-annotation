7b43004d1d1a02979b96ae059c08b15046987793
JoshR <jromphf@library.rochester.edu>
2015-09-25 15:20:56 -0400

    using this for testing login - works!

--- php/test.php
+++ php/test.php
@@ -11,8 +11,8 @@ include_once 'markerType.php';
  * @author nathans
  */
 class Test {
-    
-    
+
+
     public static function addFilm(){
         $film = new Film(null, "a film", "http://www.cnn.com");
         $updatedFilm =  FilmDAO::insert($film);
@@ -20,69 +20,69 @@ class Test {
         $numDeleted = FilmDAO::delete($film->getId());
         var_dump($numDeleted);
     }
-    
+
     public static function addUser(){
-        
-        $user = new User(null, 
-            "test@test.com", 
-            "john doe", 
+
+        $user = new User(null,
+            "test@test.com",
+            "john doe",
             User::hash("password"));
-        
+
         $updatedUser = UserDAO::add($user);
-        
+
         $emailUser = UserDAO::loginUser("test@test.com", "bad");
-        
+
         var_dump($emailUser);
-        
-      
-        
+
+
+
         $numDeleted = UserDAO::delete($updatedUser->getUserId());
-        
+
         var_dump($numDeleted);
     }
-    
+
     public static function addMarker(){
-        
+
         $film = new Film(null, "a film", "http://www.cnn.com");
-        
+
         $updatedFilm =  FilmDAO::insert($film);
-        
-        $user = new User(null, 
-            "test@test.com", 
-            "john doe", 
+
+        $user = new User(null,
+            "test@test.com",
+            "john doe",
             User::hash("password"));
         $updatedUser = UserDAO::add($user);
         $markerType = MarkerTypeDAO::getByName('Event');
-        
+
         UserDAO::addFilm($updatedUser->getUserId(), $updatedFilm->getId());
         $myVideos = UserDAO::getFilms($updatedUser->getUserId());
         var_dump($myVideos);
-        
-        
-        $array = array( "id" => NULL, 
+
+
+        $array = array( "id" => NULL,
                 "filmId" => $updatedFilm->getId(),
                 "markerId" => $markerType->getId(),
                 "start" => 0.5,
                 "end" => 0.6,
-                "text" => "this is stuff", 
-                "target" => "target", 
+                "text" => "this is stuff",
+                "target" => "target",
                 "userId" => $updatedUser->getUserId());
-        
+
         $marker = new FilmMarker($array);
         $markerWithId = FilmMarkerDAO::insertMarker($marker);
         //var_dump($markerWithId);
-        
+
         UserDAO::removeFilm($updatedUser->getUserId(), $updatedFilm->getId());
         FilmMarkerDAO::delete($markerWithId->getId());
         UserDAO::delete($updatedUser->getUserId());
         FilmDAO::delete($film->getId());
-        
 
-        
+
+
     }
-    
-   
-    
+
+
+
 }
 
 Test::addMarker();
