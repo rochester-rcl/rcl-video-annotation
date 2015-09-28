8737e7f0c35a484438b573e4dc53d69e2a76ede3
JoshR <jromphf@library.rochester.edu>
2015-09-27 21:40:11 -0400

    added userAjaxSubmit function invocation

--- app/main.js
+++ app/main.js
@@ -10,49 +10,33 @@ let footnote = require('footnote');
 let frame = 1 / 24; //Most will be 29.97 fps
 
 //Marker properties
-let filmId = 0;
-let markerId = 0;
-let start = 0;
-let text = 'blah';
-let target = '.markerControl';
-let userId = 1;
+
 
 //User properties -- base these on login - write a function in functions.js
-
-let fullName = null;
 
 
 //The main program
 
 $(document).ready( function() {
 
-  //Login
-
-  $('.user-login').submit(function(event){
-
-    let email = $('#user-email').val();
-    let password = $('#user-password').val();
-    let user = new UserAjax(email,filmId,fullName,password);
-
-    user.userLogin();
-
-  //Only working once?
-
-
-  });
-
-
+  //First we initialize our contols and player
   let popcornSelector = ".video";
 
-  let controls = new VideoController(frame, popcornSelector);
+  window.controls = new VideoController(frame, popcornSelector);
 
   controls.initControls();
 
-  UserAjax.getForm();
+  UserAjax.getForm(); //Statically calling getForm to get our buttons for the overlay
 
-  //let markerAjax = new MarkerAjax(filmId, markerId, start, text, target, userId);
+  //Login
 
+
+  functions.userAjaxSubmit(functions.logAjax);
 
 });
+
+  //console.log(markerAjax);
+  //Here a new MarkerAjax object is returned - it looks like this: let markerAjax = new MarkerAjax(filmId, markerId, start, text, target, userId);
+
 
 //1. login - change interface 2. insert 3. filter returned markers 4. delete markers
