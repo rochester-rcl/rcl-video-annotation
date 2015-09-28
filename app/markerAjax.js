ee34f18209d3c741dfa66eb6ea183f849b658eca
JoshR <jromphf@library.rochester.edu>
2015-09-27 21:42:23 -0400

    added setters/getters

--- app/markerAjax.js
+++ app/markerAjax.js
@@ -26,15 +26,43 @@ export class MarkerAjax {
     this.filmId = filmId;
   }
 
+  getFilmId() {
+    return this.filmId;
+  }
+
   setMarkerType(markerType) {
     this.markerType = markerType;
+  }
+
+  getMarkerType() {
+    return this.markerType;
   }
 
   setStart(start) {
     this.start = start;
   }
 
-  retrieveMarker() {
+  getStart() {
+    return this.start;
+  }
+
+  setText(text) {
+    this.text = text;
+  }
+
+  getText() {
+    return this.text;
+  }
+
+  setTarget(target) {
+    this.target = target;
+  }
+
+  getTarget() {
+    return this.target;
+  }
+
+  static retrieveMarkers() {
      $.ajax({
      url: 'php/pdoConnect.php',
         type: 'POST',
@@ -55,26 +83,19 @@ export class MarkerAjax {
 }
 
   insertMarker() {
-     $.ajax({
-     url: 'php/pdoConnect.php',
-        type: 'POST',
-        cache: 'false',
-        data: {'action': this.action, 'filmId': this.filmId, 'markerType': this.markerType, 'start': this.start,
-        'end': this.end, 'text': this.text, 'target': this.target, 'userId': this.target},
-        dataType:'html',
-        success: function(html) {
-                if (html) {
-                    console.log(html);
+    return $.ajax({
+      url: 'php/controllers/insert.php',
+      type: 'POST',
+      cache: 'false',
+      data: {'action': 'insert', 'email': this.email, 'password': this.password }, //film id marker start end target user id - check php object
+      dataType: 'json',
+      error: function(xhr, desc, err) {
+      console.log(xhr);
+      console.log("Details: " + desc + "\nError:" + err);
+    }
 
-                }
-            },
-        error: function(xhr, desc, err) {
-        console.log(xhr);
-        console.log("Details: " + desc + "\nError:" + err);
-        }
     });
 
-
-}
+  }
 
 }
