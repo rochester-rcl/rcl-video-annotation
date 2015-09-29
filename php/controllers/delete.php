e21ca1c2ee51b9dd561ce0dee7f95f3870c14059
JoshR <jromphf@library.rochester.edu>
2015-09-29 17:56:54 -0400

    deletes markers forever

---
+++ php/controllers/delete.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once '../marker.php';
+
+$postAction = filter_input(INPUT_POST,'action');
+
+
+if ($postAction == 'deleteMarker') {
+
+  $id = filter_input(INPUT_POST,'filmMarkerId');
+
+  FilmMarkerDAO::delete($id);
+
+  $message = [];
+
+  $message['message'] = 'Marker ' . $id . ' deleted forever.';
+
+  echo json_encode($message);
+
+} else {
+
+  echo "Couldn't connect to database";
+
+}
